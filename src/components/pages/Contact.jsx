f09bccba5e5c4eb5c72b4d1fcf612c48cdcb4464
kakeru1881 <hamiribu.kakeru@gmail.com>
2023-07-27 20:45:07 +0900

    deploy

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { init, send } from 'emailjs-com';
-import axios from "axios";
 
 export const Contact = () => {
   const [fullName, setFullName] = useState('');
@@ -13,14 +12,13 @@ export const Contact = () => {
     const userID = process.env.REACT_APP_USER_ID;
     const serviceID = process.env.REACT_APP_SERVICE_ID;
     const templateID = process.env.REACT_APP_TEMPLATE_ID;
-
+    console.log(userID);
     if (
       userID !== undefined &&
       serviceID !== undefined &&
       templateID !== undefined
     ) {
       init(userID);
-
       const template_param = {
         to_name: fullName,
         company: company,
@@ -29,7 +27,6 @@ export const Contact = () => {
       };
       send(serviceID, templateID, template_param).then(() => {
         window.alert('お問い合わせを送信致しました。');
-
         setFullName('');
         setCompany('');
         setMail('');
@@ -39,19 +36,7 @@ export const Contact = () => {
   }
   const handleClick = (e) => {
     e.preventDefault();
-    // sendMail();
-    axios.post('https://apygwgdqj4.execute-api.ap-northeast-1.amazonaws.com/v1', {
-      fullName: fullName,
-      company: company,
-      mail: mail,
-      message: message,
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    sendMail();
   };
 
   // 必須項目
@@ -61,39 +46,39 @@ export const Contact = () => {
     <SContainer>
       <STitle>Contact</STitle>
       <SForm>
-        <SInput
-          type="text"
-          value={fullName}
-          placeholder='お名前'
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        <SInput
-          type="text"
-          required
-          value={company}
-          placeholder='会社名'
-          onChange={(e) => setCompany(e.target.value)}
-        />
-        <SInput
-          type="email"
-          value={mail}
-          placeholder='メールアドレス'
-          onChange={(e) => setMail(e.target.value)}
-        />
-        <STextarea
-          type="text"
-          rows="10"
-          value={message}
-          placeholder='お問い合わせ内容を入力してください'
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <SButton
-          onClick={handleClick}
-          disabled={disableSend}
-        >
-          お問い合わせを送信する
-        </SButton>
-      </SForm>
+          <SInput
+            type="text"
+            value={fullName}
+            placeholder='お名前'
+            onChange={(e) => setFullName(e.target.value)}
+          />
+          <SInput
+            type="text"
+            required
+            value={company}
+            placeholder='会社名'
+            onChange={(e) => setCompany(e.target.value)}
+          />
+          <SInput
+            type="email"
+            value={mail}
+            placeholder='メールアドレス'
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <STextarea
+            type="text"
+            rows="10"
+            value={message}
+            placeholder='お問い合わせ内容を入力してください'
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <SButton
+            onClick={handleClick}
+            disabled={disableSend}
+            >
+            お問い合わせを送信する
+          </SButton>
+        </SForm>
     </SContainer>
   );
 }
